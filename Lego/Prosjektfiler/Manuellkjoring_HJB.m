4c98b016d26abdf3c133b32ea6919e6407c35feb
helgejo <Helge@Helges-MacBook-Air.local>
2014-09-20 22:47:23 +0200

    manuell kjøring oppdatert

--- Lego/Prosjektfiler/Manuellkjoring_HJB.m
+++ Lego/Prosjektfiler/Manuellkjoring_HJB.m
@@ -1,114 +1,50 @@
-% Maunell kjøring test HJB
+% Maunell kj?ring test HJB
 
 %% Initialiserer NXT
-COM_CloseNXT all                % lukker alle NXT-håndtak 
+COM_CloseNXT all                % lukker alle NXT-h?ndtak 
 close all                       % lukker alle figurer  
 clear all                       % sletter alle variable 
-handle_NXT = COM_OpenNXT();     % etablerer nytt håndtak
-COM_SetDefaultNXT(handle_NXT);	% setter globalt standard-håndtak
-
-%% Initialiser/åpne sensorer
-%
-% Les mer om hvordan disse initialiseres/åpnes i dokumentet: 
-%
-% #2 Introduksjon til MATLAB, LEGO NXT og RWTH Mindstorms toolbox
-%
-OpenSound(SENSOR_2,'DB');% Lydsensor i inngang 2
-
+handle_NXT = COM_OpenNXT();     % etablerer nytt h?ndtak
+COM_SetDefaultNXT(handle_NXT);	% setter globalt standard-h?ndtak
 
 %% Initialiserer motor A
-%
-% Les mer om forskjellige motoregenskap og hvordan disse initialiseres
-% i dokumentet: 
 %  #2 Introduksjon til MATLAB, LEGO NXT og RWTH Mindstorms toolbox
 %
-motorA = NXTMotor('A','SmoothStart',true); % initialiserer motor A
+motorB = NXTMotor('B','SmoothStart',true); % initialiserer motor B
+motorC = NXTMotor('C','SmoothStart',true); % initialiserer motor C
 
 
 %% Initialiserer joystick
-joymex2('open',0);                     % åpner joystick
-joystick      = joymex2('query',0);    % spør etter data fra joystick
+joymex2('open',0);                     % ?pner joystick
+joystick      = joymex2('query',0);    % sp?r etter data fra joystick
 JoyMainSwitch = joystick.buttons(1);   % henter verdien fra knapp nr 1 som er stoppknappen 
-
-
-%% Definerer vektorer bestående av 100 elementer med 0'er
-% for hver inngang, utgang og egendefinerte størrelser. 
-% Grunnen til 0'en er at vi til enhver tid plotter siste 90 elementer.
-% Her kan dere fylle på med de variable dere ønsker å lagre
-
-% Innganger, f.eks. lyd
-Lyd        = zeros(1,100);  % lagrer lydsignal
-
-% Utganger
-PowerA     = zeros(1,100);  % lagrer motorpådrag
-
-% Egendefinerte vektorere
-JoyForover = zeros(1,100);  % lagrer bevegelsen til joystick
-
-
-%% Initialiser figurer
-set(0,'DefaultFigureUnits','normalized')
-figure('Position',[0.01 0.51 0.4 0.4])
-figure('Position',[0.44 0.51 0.4 0.4])
-
 
 %% index for lagring i vektorer
 i=1;
 
 while ~JoyMainSwitch
-    %% få tak i joystickdata
-    joystick        = joymex2('query',0);       % spør etter data fra joystick
+    %% f? tak i joystickdata
+    joystick        = joymex2('query',0);       % sp?r etter data fra joystick
     JoyMainSwitch   = joystick.buttons(1);
     JoyForover(i)   = -joystick.axes(2)/327.68; % 32768 fremover, -32768 bakover
-
     
-    %% få tak i nye sensordata og lagre i vektor
-    Lyd(i) = GetSound(SENSOR_2);
-
-    
-    %% beregner motorpådrag og lagrer i vektir
-    PowerA(i) = JoyForover(i);
-
+    %% beregner motorpaadrag og lagrer i vektor
+    PowerB(i) = JoyForover(i);
+    PowerC(i) = JoyForover(i);
     
     %% set output data
-    motorA.Power = PowerA(i);
-    motorA.SendToNXT();
-    
-    
-    %% plot data
-    figure(1)
-    subplot(2,1,1)
-    bar(PowerA(i));
-    axis([0 1 -100 100])
-    title('Pådrag motor A')
-
-    subplot(2,1,2)
-    plot(JoyForover(end-90:end));
-    axis([0 100 -100 100])
-    title('Pådrag motor A som funksjon av tid')
-
-    figure(2)
-    subplot(2,1,1)
-    plot(Lyd(end-90:end));
-    title('Lydverdi')
-
-    subplot(2,1,2)
-    plot(JoyForover(end-90:end),'r');
-    hold on
-    plot(Lyd(end-90:end),'b');
-    axis([0 100 -Inf Inf])
-    title('Motorpådrag (rød) og lyd (blå) i samme plot')
-    hold off
-    
-    % tegn nå
-    drawnow
+    motorB.Power = PowerB(i);
+    motorC.Power = PowerC(i);
+    motorB.SendToNXT();
+    motorC.SendToNXT();
     
     % oppdaterer tellevariabel
     i=i+1;
 end
 
 %% stopp motorer
-motorA.Stop;
+motorB.Stop;
+motorC.Stop;
 
 % Clear MEX-file to release joystick
 clear joymex2
