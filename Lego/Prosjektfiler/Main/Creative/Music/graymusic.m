a2d4b348e314b26b5c72f6a9b815736d5bc4cc08
helgejo <Helge@Helges-MacBook-Air-4.local>
2014-11-12 23:11:08 +0100

    changez
    Oppdaterte kommentarer på joymusic og graymusic

--- Lego/Prosjektfiler/Main/Creative/Music/graymusic.m
+++ Lego/Prosjektfiler/Main/Creative/Music/graymusic.m
@@ -1,11 +1,15 @@
 function [] = graymusic()
-% Kreativ oppgave av Helge Bjorland
+%% Kreativ oppgave av Helge Bjorland
 
-%Bruk lysm?ler til ? spille musikk
-%   Bruk lysm?ler til ? lese gr?toner p? et eller flere ark, hvor gr?tone
+%% Bruk lysm?ler til ? spille musikk
+%   Bruker lysm?ler til ? lese gr?toner separert av hvitt felt p? ett ark, hvor gr?tone
 %   repr. en tone, og den fysiske lengden av gr?tonene p? arket repr.
-%   tidslengde som hver tone skal spilles. La roboten kj?re over arket og
-%   spille melodien
+%   tidslengde som hver tone skal spilles. Man velger: 
+%       + st?rrelse p? arket
+%       + Lyd lengde multiplier
+%       + Robot fart under m?ling
+%       + Presisjon p? lysm?ling
+%   Roboten kj?rer over arket og spiller deretter melodien
 
 %% Initialiserer NXT
 initNXT();
@@ -13,31 +17,31 @@ handle_NXT = COM_OpenNXT();     % etablerer nytt h?ndtak
 COM_SetDefaultNXT(handle_NXT);	% setter globalt standard-h?ndtak
 
 %% Initialiserer sensorer
-OpenLight(SENSOR_3,'ACTIVE'); %Lys sensor
+OpenLight(SENSOR_3,'ACTIVE');   % ?pner lys sensor
 
 %% Initialiserer motorer
 
-motorB = NXTMotor('B','SmoothStart',true); % Init motor b (h?yre)
-motorC = NXTMotor('C','SmoothStart',true); % Init motor c (venste)
+motorB = NXTMotor('B','SmoothStart',true); % Initialiser motor b (h?yre)
+motorC = NXTMotor('C','SmoothStart',true); % Initialiser motor c (venste)
 
 %% Initialiser variabler
-lys = 0; %m?ling fra lyssensor
-lysFilt=0; %filtrert lysm?ling
-RoboSpeed = 10; %Robot speed m/s
-TotLen = 300; % Total length robot should drive
-LenRead=5;
-SoundLen= 10;
-RoboPrec= 5;
-Mlen= 0;
-Mtone= 200;
-templys = 0;
-lysneg = 0;
-i=1;
-danceDirect=1;
-danceSpeed=15;      
+lys = 0;            % m?ling fra lyssensor
+lysFilt=0;          % filtrert lysm?ling
+RoboSpeed = 10;     % Robot speed m/s
+TotLen = 300;       % Total length robot should drive
+LenRead=5;          % M?ler lengden robot har kj?rt
+SoundLen= 10;       % Lengden p? tonen i forhold til distanse
+RoboPrec= 5;        % Presisjons faktor for lysm?ler
+Mlen= 0;            % Tone lengde
+Mtone= 200;         % Tone frekvens
+templys = 0;        % lysm?ling temp variabel
+lysneg = 0;         % lysm?ling derivert positiv eller negativ
+i=1;                % teller i while loop
+danceDirect=1;      % Danse retning
+danceSpeed=15;      % Danse fart
 
 function [TotLen,SoundLen,RoboSpeed,RoboPrec]=menuCh()
-        % Menu Choices
+        % Meny for ? sette papirst?rrelse
 
         switch menu('Choose length robot should drive to measure: ', 'A3', 'A4', 'A5','Back')
             case 1
@@ -49,26 +53,25 @@ function [TotLen,SoundLen,RoboSpeed,RoboPrec]=menuCh()
             case 4
                 main();
         end
-prompt={'Sound length multiplier? Default is 1 (1cm=1ms). Higher number give longer tones',...
+        
+% Meny for ? sette variabler fra bruker input
+prompt={'Sound length multiplier? Default is 1 (1mm=1ms). Higher number give longer tones',...
         'Input robot speed when measuring? Default is 10 and maks is 100',...
         'Input robot light reading precision between 0 - 100? Lower number is more accurate. Default is 5'};
 name='Input for light to music function';
 numlines=1;
 defaultanswer={'1','10','5'};
 answer=inputdlg(prompt,name,numlines,defaultanswer);
-        %Lenprompt = 'How long in ms should the sound play per cm? Default is 1000 (1cm=1s). Higher number give longer tones';
         SoundLen = str2double(answer(1))*10;
             if isempty(SoundLen)
                 SoundLen = 10;
             end
             
-        %Speedprompt = 'Input robot speed? Default is 10 and maks is 100';
-        RoboSpeed = str2double(answer(2)); 
+        RoboSpeed = str2double(answer(2));
             if or(isempty(RoboSpeed),RoboSpeed > 100) 
                 RoboSpeed = 10;
             end
 
-        %Precprompt = 'Input robot light reading precision between 0 - 100? Lower number is more accurate. Default is 75';
         RoboPrec = str2double(answer(3)); 
             if or(isempty(RoboPrec),RoboPrec > 360) 
                 RoboPrec = 5;
@@ -77,37 +80,37 @@ answer=inputdlg(prompt,name,numlines,defaultanswer);
     end
     
 %% Getting everything ready
-[TotLen, SoundLen, RoboSpeed,RoboPrec] = menuCh(); %get user input
-motorB.ResetPosition(); % nullstill vinkelteller
-motorC.ResetPosition(); % nullstill vinkelteller
-lys = GetLight(SENSOR_3); % Get first light reading
-screen = get(0,'screensize');
+[TotLen, SoundLen, RoboSpeed,RoboPrec] = menuCh();      % get user input
+motorB.ResetPosition();                                 % nullstill vinkelteller
+motorC.ResetPosition();                                 % nullstill vinkelteller
+lys = GetLight(SENSOR_3);                               % Get first light reading
+screen = get(0,'screensize');                           % hent skjermst?rrelse for ? stille grafene
 
-%Main figur, Totalt avvik
-figure('Name','Hovedfigur - Press en tast for å komme tilbake til meny','Position',[screen(3)/8, screen(4)/8,3*screen(3)/8, 2.8*screen(4)/8],'NumberTitle','off');
+% Figur for ? vise tonelengde
+figure('Name','Hovedfigur - Press en tast for ? komme tilbake til meny','Position',[screen(3)/8, screen(4)/8,3*screen(3)/8, 2.8*screen(4)/8],'NumberTitle','off');
 subplot(3,1,1)
 plot1_1=plot((1:(length(Mlen))), Mlen);
 set(plot1_1,'Ydata', Mlen , 'Xdata', (1:(length(Mlen))) );
 hold on;
 title('Lyd data');
-
-% xlabel('Målepunkt');
 ylabel('Lydlengde');
+
+% Figur for ? vise frekvens
 subplot(3,1,2)
 plot1_2=plot((1:(length(Mtone))), Mtone);
 set(plot1_2,'Ydata', Mtone , 'Xdata', (1:(length(Mtone))) );
-
-%xlabel('Målepunkt');
 ylabel('Frekvens');
 hold on;
+
+% Figur for ? vise lysm?lingsdata
 subplot(3,1,3)
 plot1_3=plot((1:(length(lysFilt))), lysFilt);
 set(plot1_3,'Ydata', lysFilt , 'Xdata', (1:(length(lysFilt))) );
-xlabel('Målepunkt');
+xlabel('M?lepunkt');
 ylabel('Lys');
 hold on;
 
-% move forward with robospeed   
+% Move forward with robospeed   
 motorB = NXTMotor('B','Power',RoboSpeed);
 motorC = NXTMotor('C','Power',RoboSpeed);
 motorB.SendToNXT();
@@ -115,79 +118,77 @@ motorC.SendToNXT();
     
 %% Drive and collect measurments
 while LenRead < TotLen;
-    % when lightsensor detects a major change larger than RoboPrec then 
-    % get motor position and calculate distance
-    % Play tone for distance and light(end-1)
-    data = motorB.ReadFromNXT(); % hent motorinformasjon 
-    lys(end+1)=GetLight(SENSOR_3);
-    lysFilt(end+1)=Lfilter([lysFilt(end),lys(end)], RoboPrec);
-    deltaLys = lysFilt(end)-lysFilt(end-1);
+    data = motorB.ReadFromNXT();                                    % hent motor B informasjon 
+    lys(end+1)=GetLight(SENSOR_3);                                  % hent lysm?ling
+    lysFilt(end+1)=Lfilter([lysFilt(end),lys(end)], RoboPrec);      % filtrer lysm?ling med input presision
+    deltaLys = lysFilt(end)-lysFilt(end-1);                         % Finn endring i lysm?ling
 
-    if deltaLys < 0;
-        templys(end+1) = lysFilt(end);
-        lysneg = 1;
-    elseif deltaLys == 0;
-        if lysneg == 1;
-            templys(end+1) = lysFilt(end);
+    if deltaLys < 0;                                                % Hvis endring i lysm?ling er negativ (et m?rkere parti starter)
+        templys(end+1) = lysFilt(end);                              % registrer da lysm?ling i temp variabel
+        lysneg = 1;                                                 % sett lysneg til 1 for ? vite at m?ling av gr?tt omr?de er startet
+ 
+    elseif deltaLys == 0;                                           % Hvis det ikke er endring i lysm?ling
+        if lysneg == 1;                                             % Men man er p? et gr?tt felt
+            templys(end+1) = lysFilt(end);                          % registrer lysm?ling
         end
     else
-       if lysneg == 1;
-           distdeg = motorC.ReadFromNXT();
-           Mtone(end+1) = LtoHZ(mean(templys));
-           Mlen(end+1)= Deg2Dist(distdeg.Position)*SoundLen;
-           motorC.ResetPosition(); % nullstill vinkelteller
-           templys = 0;
-           lysneg = 0;
+       if lysneg == 1;                                              % Endring er positiv og dette er f?rste gang (hvitt parti starter)                              
+           distdeg = motorC.ReadFromNXT();                          % Les data fra motor C
+           Mtone(end+1) = LtoHZ(mean(templys));                     % Sett lyd tone til gjennomsnitt lysm?linger som er gjort p? gr?tt parti
+           Mlen(end+1)= Deg2Dist(distdeg.Position)*SoundLen;        % Sett lengden p? tone til fysisk lengde av gr?tt p? arket fra motor c m?ling
+           motorC.ResetPosition();                                  % nullstill motorC posisjons m?ler
+           templys = 0;                                             % T?m temp lysm?lingsvariabel
+           lysneg = 0;                                              % Forteller at robot er p? hvitt parti og gr?tt omr?de er allerede registrert
        end
     end
-    LenRead = Deg2Dist(data.Position);
+    LenRead = Deg2Dist(data.Position);                              % Sjekk total kj?relengde for ? se om det er p? tide ? stoppe
 end
 
-% Stop motorer
+% Stop motorer n?r ark lengde er n?dd
 motorB.Stop;
 motorC.Stop;
 
-%% Play music and dance
-while i < length(Mlen);
-    NXT_PlayTone(Mtone(i),Mlen(i));
-
+%% Spill av sangen og dans til de herlige tonene
+while i < length(Mlen);                                         % Kj?r til alle toner er spillt av
+    NXT_PlayTone(Mtone(i),Mlen(i));                             % Spill toner p? nxt
     %Do a boogie to the lovely music
-    motorB = NXTMotor('B','Power',danceSpeed*danceDirect);
-    motorC = NXTMotor('C','Power',danceSpeed*danceDirect*-1);
-    motorB.SendToNXT();
-    motorC.SendToNXT();
-    danceDirect = danceDirect * -1;
-    pause((Mlen(i))/1000)
-    i=i+1;
+    motorB = NXTMotor('B','Power',danceSpeed*danceDirect);      % kj?r motor b i en retning
+    motorC = NXTMotor('C','Power',danceSpeed*danceDirect*-1);   % kj?r motor c i andre retning
+    motorB.SendToNXT();                                         % send motorinfo til nxt
+    motorC.SendToNXT();                                         % send motorinfo til nxt
+    danceDirect = danceDirect * -1;                             % snu motor retninger
+    pause((Mlen(i))/1000)                                       % pause i tonelengde for ? la roboten spille ferdig tone
+    i=i+1;                                                      % ?k teller
 end
 
 
-%% Avslutt
+%% Avslutt n?r sang og dans er ferdig
 % Stop motorer
 motorB.Stop;    
 motorC.Stop;
 
+%% Oppdater figurer
 figure(1) 
     set(plot1_1,'Ydata', Mlen ,     'Xdata',  (1:(length(Mlen))));
     set(plot1_2,'Ydata', Mtone ,    'Xdata',  (1:(length(Mtone))));
     set(plot1_3,'Ydata', lysFilt ,  'Xdata',  (1:(length(lysFilt))) );
 
-% Steng kobling til sensorer
+%% Steng koblinger til sensorer og avslutt n?r knapp trykkes p? keyboard
 CloseSensor(SENSOR_3);
 
 w = 2;
-    h=0;
-    if h==0
-        h= msgbox('Press any key to close figures and return to menu','Message','help')
-    end
+h=0;
+if h==0
+    h= msgbox('Press any key to close figures and return to menu','Message','help')
+end
 
-    while w > 1
+while w > 1
     w = waitforbuttonpress;
         if w <= 1;
             % Steng NXT tilkobling
             initNXT();
             % Return to main menu
-            %main();
+            % main();
         end
     end
 
--- Lego/Prosjektfiler/Main/Creative/Music/joymusic.m
+++ Lego/Prosjektfiler/Main/Creative/Music/joymusic.m
@@ -2,32 +2,32 @@ function [] = joymusic()
 %JOYMUSIC spiller musikk med joystick
 
 %% Initialiserer variabler
-run = true; % loop variabel, settes til false for ? avslutte programmet
-Mlen = 50; %ms the tone should last for
-joyFB = 0;                                      % vektor for forover/bakover bevegelse av joystick
-joyS = 0;                                       % vektor for sideveis bevegelse av joystick
+run = true;     % loop variabel, settes til false for ? avslutte programmet
+Mlen = 50;      % ms the tone should last for
+joyFB = 0;      % vektor for forover/bakover bevegelse av joystick
+joyS = 0;       % vektor for sideveis bevegelse av joystick
 Mtone = 0;
-   %% Initialiserer NXT
-    initNXT();
-    handle_NXT = COM_OpenNXT();     % etablerer nytt håndtak
-    COM_SetDefaultNXT(handle_NXT);	% setter globalt standard-håndtak
+%% Initialiserer NXT
+initNXT();
+handle_NXT = COM_OpenNXT();     % etablerer nytt h?ndtak
+COM_SetDefaultNXT(handle_NXT);	% setter globalt standard-h?ndtak
 
 %% Initialiserer joystick
-joymex2('open',0);                  % ?pner joystick
-joystick = joymex2('query',0);      % sp?r etter data fra joystick
-JoyMainSwitch = joystick.buttons(1);   % Knapp 1, for ? stoppe program
+joymex2('open',0);                      % ?pner joystick
+joystick = joymex2('query',0);          % sp?r etter data fra joystick
+JoyMainSwitch = joystick.buttons(1);    % Knapp 1, for ? stoppe program
 
 while run
-    joystick = joymex2('query',0);
-    joyFB(end+1) = (-joystick.axes(2))/(32.768); % henter joystick posisjon p? "y"-aksen som angir lave toner
-    joyS(end+1) = ((joystick.axes(1))/(32.768))*2; % henter joystick posissjon p? "x"-aksen som angir h?ye toner
-    if ((joyFB(end)+joyS(end))/2) > 0;
-        Mtone = LtoHZ((joyFB(end)+joyS(end))/2);
-        NXT_PlayTone(Mtone, Mlen);
+    joystick = joymex2('query',0);                  % sp?r etter data fra joystick
+    joyFB(end+1) = (-joystick.axes(2))/(32.768);    % henter joystick posisjon p? "y"-aksen som angir lave toner
+    joyS(end+1) = ((joystick.axes(1))/(32.768))*2;  % henter joystick posissjon p? "x"-aksen som angir h?ye toner
+    if ((joyFB(end)+joyS(end))/2) > 0;              % Hvis joystick posisjon st?rre enn 0
+        Mtone = LtoHZ((joyFB(end)+joyS(end))/2);    % Konverter joystick signal til hz
+        NXT_PlayTone(Mtone, Mlen);                  % Spill tone
     end
-    JoyMainSwitch   = joystick.buttons(1);
+    JoyMainSwitch   = joystick.buttons(1);          % Definer avsluttings program
     if JoyMainSwitch
-        run = false;
+        run = false;                                % Hvis knapp trykket avsluttes programmet
     end
 end
 
