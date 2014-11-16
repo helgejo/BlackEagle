597e05a2f64b7327ee169e0f6f8438f1938c89a2
lovikd <danielloviks@gmail.com>
2014-11-16 21:14:06 +0100

    Update

--- Lego/Prosjektfiler/Main/Creative/RoboFun/LydSensorClap.m
+++ Lego/Prosjektfiler/Main/Creative/RoboFun/LydSensorClap.m
@@ -21,32 +21,34 @@ function []=lydsensorclap()
 
 
     %% Initialiserer Lyd sensor
-    %
+    OpenUltrasonic(SENSOR_4);
     OpenSound(SENSOR_2, 'DB' ); %  Åpner lydsensor og leser data i DB
 
 
     %% Div variabler
     slyd = [0];% Lydvektor
+    sUltra = [0];% Ultravektor
     tid = [0]; % Tidsvektor
     Progstart = true; % Variable for While kontroll
     Fremover = NXTMotor('BC') ; % Kjører frem
     clapcount = 0; % Klappteller
     clapreg = false; % Klapregistrering
     claptime = [1] ; % Tidsvektor for bruk i cputime kode til måling mellom klapp
+    Start = cputime; % Starttid
 
 
     while Progstart
             % Plotter nåværende lyd verdier og ultraverdier
-            tid(end+1)=cputime-ProgStart;
+            tid(end+1)=cputime-Start;
             slyd(end+1) = GetSound(SENSOR_2);
-            figure(1)
+            subplot(2,1,1)
             plot(tid,slyd);
             title('Måling av lydnivå')
             ylabel({'Lydverdi'})
             xlabel({'Tid'});
             sUltra(end+1) = GetUltrasonic(SENSOR_4);
             subplot(2,1,2)
-            plot(sUltra);
+            plot(tid,sUltra);
             title('Ultralyd måling');
             ylabel({'Ultraverdi'});
             xlabel({'Tid'});
@@ -77,16 +79,16 @@ function []=lydsensorclap()
             % Ved 1 vanlig klapp og 2 kjappe klapp settes NXT i brems.
            if clapcount == 3   
                     Fremover.Stop('brake')
-                    posdata = Fremover.ReadFromNXT(); % Leser antall grader hjul har rotert.
 
-                    disp(sprintf('NXT har kjørt %d', posdata.Position)); % Viser antall grader.
-                    %Mulighet for formel for å vise distanse i cm.
 
                   % Ved nytt klapp etter 3 resettes klappteller og starter på 1.  
            elseif clapcount > 3
                   clapcount = (clapcount - clapcount) +1  
 
            end
+           if GetUltrasonic(SENSOR_4) < 25
+               Fremover.Stop('off');
+               Progstart = false;
 
     end
 end
